ae5b5683dad9c97aecbc861c2870626444d75732
Vinicius de Lucena <viniciusmelosegundo@gmail.com>
2023-09-11 16:43:00 -0300

    Finalizando classe conta bancaria da atividade 2

---
+++ src/Exercicio2POO/ContaBancaria.java
@@ -0,0 +1,91 @@
+import java.time.LocalTime;
+
+public class ContaBancaria {
+    private String numeroConta;
+    private String titular;
+    private double saldo;
+    private double limiteChequeEspecial;
+    private LocalTime horarioLimiteSaqueInicio;
+    private LocalTime horarioLimiteSaqueFim;
+
+    public ContaBancaria(String numeroConta, String titular, double saldo, double limiteChequeEspecial) {
+        this.numeroConta = numeroConta;
+        this.titular = titular;
+        this.saldo = saldo;
+        this.limiteChequeEspecial = limiteChequeEspecial;
+        this.horarioLimiteSaqueInicio = LocalTime.of(7, 0); // Horário de início dos saques permitidos (9:00)
+        this.horarioLimiteSaqueFim = LocalTime.of(18, 0);   // Horário de término dos saques permitidos (18:00)
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }    
+    public String getTitular() {
+        return titular;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void deposito(double valor) {
+        this.saldo = getSaldo() + valor;
+    }
+    public void saque(double valor) throws ContaBancaria.saldoIndisponivelException, ContaBancaria.horarioIndisponivelException {
+        LocalTime agora = LocalTime.now();
+        
+        // Verifica se o horário atual está dentro do horário permitido para saques
+        if (agora.isBefore(horarioLimiteSaqueInicio) || agora.isAfter(horarioLimiteSaqueFim) && valor > 100.0) {
+            throw new horarioIndisponivelException("O horário não permite saque");
+        }
+
+        // Verifica se o valor do saque é maior que o saldo disponível
+        if (valor > (saldo + limiteChequeEspecial)) {
+            throw new saldoIndisponivelException("Não há saldo suficiente");
+        }
+
+        // Aplica tarifa de juros para saques do cheque especial
+        if (valor > saldo) {
+            double juros = 0.1 * (valor - getSaldo());
+            saldo -= valor + juros; // 10% de juros
+        } else {
+            saldo -= valor;
+        }
+    }
+
+    public class saldoIndisponivelException extends Exception {
+        public saldoIndisponivelException(String message) {
+            super(message);
+        }
+    }
+    public class horarioIndisponivelException extends Exception {
+        public horarioIndisponivelException(String message) {
+            super(message);
+        }
+    }
+
+    public static void main(String[] args) throws ContaBancaria.saldoIndisponivelException, ContaBancaria.horarioIndisponivelException {
+        ContaBancaria conta1 = new ContaBancaria("12345-6", "Richard", 100.00, 5000.00);
+         
+        System.out.println("\nOlá, " + conta1.getTitular() + " seu saldo é: R$ " + conta1.getSaldo() + "\n");
+
+        conta1.deposito(1000.00);
+        System.out.println("Deposito de R$ 1000");
+        System.out.println(conta1.getTitular() + " seu saldo é: R$ " + conta1.getSaldo() + "\n");
+
+        conta1.saque(600.0);
+        System.out.println("Saque de R$ 600");
+        System.out.println(conta1.getTitular() + " seu saldo é: R$ " + conta1.getSaldo() + "\n");
+
+        System.out.println("Saque de R$ 2000");
+        conta1.saque(2000.0);
+        System.out.println(conta1.getTitular() + " seu saldo é: R$ " + conta1.getSaldo() + "\n");
+
+        System.out.println("Saque de R$ 2000");
+        conta1.saque(2000.0);
+        System.out.println(conta1.getTitular() + " seu saldo é: R$ " + conta1.getSaldo() + "\n");
+
+        System.out.println("Saque de R$ 2000");
+        conta1.saque(2000.0);
+        System.out.println(conta1.getTitular() + " seu saldo é: R$ " + conta1.getSaldo() + "\n");
+    }
+}
