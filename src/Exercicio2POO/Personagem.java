72adb48ab1a6b04a3214769b03bb652b7f0fc697
Richard Ferreira Salviano <salviano.richard@academico.ifpb.edu.br>
2023-09-14 23:39:56 -0300

    Add files via upload

---
+++ src/Exercicio2POO/Personagem.java
@@ -0,0 +1,35 @@
+public class Personagem {
+    private String nome;
+    private int saude;
+    private int experiencia;
+
+    public Personagem(String nome, int saude, int experiencia) {
+        this.nome = nome;
+        this.saude = saude;
+        this.experiencia = experiencia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public int getSaude() {
+        return saude;
+    }
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+    public void sofrerDano() {
+
+    }
+    public void recuperarSaude() {
+
+    }
+    public void ganharExperiencia() {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
